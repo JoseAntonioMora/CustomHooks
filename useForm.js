87b6b78770b28ab18c3b59c9f9e06e43caf01e4a
Jose Antonio Mora <j_antonio_mora@hotmail.com>
2020-10-15 13:37:53 -0500

    primer Commit

---
+++ useForm.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+// Custom Hook para campos de texto en formulario
+
+export const useForm = (initialState = {}) => {
+
+	const [values, setValues] = useState(initialState);
+
+	const reset = () => {
+		setValues( initialState );
+	}
+
+	const handleInputChange = ({ target }) => {
+		setValues({
+			...values,
+			[ target.name ] : target.value
+		})
+	}
+
+	return [values, handleInputChange, reset ];
+}
